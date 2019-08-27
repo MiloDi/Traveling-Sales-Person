55dcf62c09a8a3965c6b5508918162033d93b01f
Milo <milodietrick@gmail.com>
2019-08-27 13:50:14 -0600

    Clean up code add docs

--- tsp.py
+++ tsp.py
@@ -1,4 +1,5 @@
 # Author: Milo Dietrick
+# Purpose: Nearest neighbhors and exhaustive approach for the traveling sales person problem
 
 from pathlib import Path
 import timeit
@@ -6,77 +7,84 @@ import math
 import itertools
 import random
 
-# code for timer found at
-# https://stackoverflow.com/questions/15707056/get-time-of-execution-of-a-block-of-code-in-python-2-7
-# start_time = timeit.default_timer()
-# print("K nearest time:" timeit.default_timer() - start_time)
-def calculate_distance( l1, l2):
-   return pow( pow((l1[0]-l2[0]), 2) + pow((l1[1] - l2[1]),2), .5 ) 
 
-
-# #TESTING POINTS FROM INPUT FILE
-# initial =[[0,0], [10,0], [0,5], [10,5]]
-
-
-#GENERATE RANDOM POINTS
-initial=[]
-for i in range (16):
-  a = [random.randint(1,11), random.randint(1,11)]
-  initial.append(a)
-
-
-c=initial.copy()
-#NEAREST NEIGHBHOR HEURISTIC
-best_order = [initial[0]]
-total_distance = 0.0
-
-print("\n order before nearest tsp: ", initial)
-
-j=0
-# length = len(b) -2
-initial.pop(0)
-
-start_time = timeit.default_timer()
-while len(initial) > 0:
-    #calculate the closest point
-    dist = []    
-    for i in initial:
-        dist.append (calculate_distance( best_order[j], i)) #calculate distance     
-    total_distance += min(dist)  
-    best_order.append(initial[dist.index(min(dist))]) #find the point with the shortest distance   
-
-    if len(initial) == 1: #if its the last iteration add the starting point to the end of the list
-        total_distance += calculate_distance(best_order[0], initial[0])
-        best_order.append(best_order[0])
-    initial.pop(dist.index(min(dist)))
-    j+=1 
-end_time = timeit.default_timer() - start_time
-print("\n K_nearest \n total distance", total_distance)
-print("best order", best_order)
-print("K nearest time:", end_time)
-
-
-#EXHAUSTIVE
-initial = c[0]
-c.pop(0)
-dist = []
-y = []
-
-start_time = timeit.default_timer()
-for x in itertools.permutations(c,len(c)):
-  total_distance = 0
-  x=list(x)
-  x.insert(0, initial)
-  x.insert(len(x), initial)
-  for i in range (len(x) - 1):
-    distance = calculate_distance(x[i], x[i+1])
-    total_distance += distance
-  dist.append(total_distance)
-  y.append(x)
-end_time = timeit.default_timer() - start_time
-print("\n Exhaustive approach")
-print("Best order: ", y[dist.index(min(dist))])
-print("Shortest dsitance",  min(dist))
-print("Exhaustive time:", end_time)
+def calculate_distance(point1, point2):
+  """Caclulate the Euclidian distance between 2 points
   
+  Arguments:
+      point1 {List} -- Starting x permutations coordinate
+      point2 {List} -- Ending x permutations coordinate
+  
+  Returns:
+      [int] -- Distance between 2 points
+  """
+  return pow(pow((point1[0] - point2[0]), 2) + pow((point1[1] - point2[1]), 2), .5) 
+
+def generate_random_points(numPoints, maxRange):
+  """Generate random x, permutations coordinates
+  
+  Arguments:
+      numPoints {int} -- Number of points to generate
+      maxRange {int} -- Maximum range of the points
+  
+  Returns:
+      List -- List containing lists of x, permutations coordinates
+  """
+  return [[random.randint(1, maxRange), random.randint(1, maxRange)] for i in range (numPoints)]
+
+def nearest_neighbhor(points):
+  """Finds the shortest route using the nearest neighbhors hueristic
+  
+  Arguments:
+      points {List} -- List of points that must be visited
+  """
+  print("\nNearest neigbhor search \nOrder before search: ", points)
+  counter=0
+  best_order = [points[0]]
+  points.pop(0)
+  total_distance = 0.0
+  start_time = timeit.default_timer()
+  while len(points) > 0:
+      dist = [(calculate_distance( best_order[counter], i)) for i in points]  
+      total_distance += min(dist)   
+      best_order.append(points[dist.index(min(dist))]) 
+      #if its the last iteration add the starting point to the end of the list
+      if len(points) == 1: 
+          total_distance += calculate_distance(best_order[0], points[0])
+          best_order.append(best_order[0])
+      points.pop(dist.index(min(dist)))
+      counter+=1
+  #measure runtime    
+  end_time = timeit.default_timer() - start_time
+  print("Best order", best_order,"\nShortest distance", total_distance,"\nNearest Neighbhor time:", end_time)
+
+
+def exhaustive_search(points):
+  """Finds the shortest path using a exhaustive approach through each permutation
+  
+  Arguments:
+      points {List} -- List of points that must be visited
+  """
+  initial = points[0]
+  permutations = []
+  print("\nExhaustive Search\nOrder before search: ", points)
+  start_time = timeit.default_timer()
+  for perm in itertools.permutations(points, len(points)):
+    perm=list(perm)
+    #ensure that the path goes back to the starting point
+    perm.insert(0, initial)
+    perm.insert(len(perm), initial)
+    dist = [ calculate_distance(perm[i], perm[i+1]) for i in range (len(perm) - 1)]
+    permutations.append(perm)
+  #measure runtime
+  end_time = timeit.default_timer() - start_time
+  print("Best order: ", permutations[dist.index(min(dist))], "\nShortest distance",  min(dist), "\nExhaustive time:", end_time)\
     
+  
+if __name__ =='__main__':
+  numPoints = input("How many points to run? ")
+  maxRange = input("Maximum range of points: ")
+  point_list = generate_random_points(int(numPoints), int(maxRange))
+  
+  nearest_neighbhor(point_list.copy())
+  exhaustive_search(point_list.copy())
